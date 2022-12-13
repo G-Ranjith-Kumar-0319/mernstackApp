39c2c9a09e5cef76b06729380234ce745e436005
Ranjith <34737524+G-Ranjith-Kumar-0319@users.noreply.github.com>
2022-12-13 08:48:39 +0530

    dev
    dev

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2bafcb48103de29a"; }
+/******/ 		__webpack_require__.h = function() { return "ddcf1b0b24416e39"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
